96de20adcfbb7d45614a541542e0036d1b0dc94f
George <89341385+Georgxe@users.noreply.github.com>
2021-08-23 01:55:51 +1000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,8 +4,8 @@ module.exports = {
 	execute(message, args) {
         const { MessageEmbed } = require('discord.js');
         const embed = new MessageEmbed()
-        .addField("Invite The bot to your server.", "[Bot Invite](https://discord.com/api/oauth2/authorize?client_id=858263515202322483&permissions=0&scope=bot)")
-        .addField("Have Errors? Report them here.", "[Server Invite](https://discord.gg/7CKdtBut)")
+        .addField("Invite The bot to your server.", "[Bot Invite](https://discord.com)")
+        .addField("Have Errors? Report them here.", "[Server Invite](https://discord.gg/d)")
         .setThumbnail("https://cdn.discordapp.com/avatars/858263515202322483/83b8c624ac15c473ca675f63d0a24be6.webp?size=256")
         .setColor(0)
 		message.channel.send(embed);
